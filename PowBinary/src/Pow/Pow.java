705c7f4a03d66b0b50f32f70a928002dd20b1f4f
rsltgy <rsltgy@DESKTOP-4U5J8L8>
2016-05-02 22:00:25 +0300

    a^b mod m

---
+++ PowBinary/src/Pow/Pow.java
@@ -0,0 +1,25 @@
+package Pow;
+
+/*
+ * Author : Resul Tugay
+ * */
+public class Pow {
+
+	public static void main(String[] args) {
+		System.out.println(pow_binary(100,5000,37));
+	}
+	
+	public static int pow_binary(int base,int exp,int modulo){
+		int r = 1;
+		base = (base % modulo);
+		while(exp > 0){
+			int x = exp % 2;
+			if(x > 0){
+				r = (r * base) % modulo;
+			}
+			exp = exp / 2;
+			base = (base * base) % modulo;
+		}
+		return r;
+	}
+}
